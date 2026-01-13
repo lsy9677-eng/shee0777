1c047f10a23b401c69fd403c80ffa671f4e6be02
lsy9677-eng <lsy9677@gmail.com>
2026-01-13 16:34:07 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'namyang-math-v3';
+
+const urlsToCache = [
+  './',
+  './index_sms_aligo.html',
+  './manifest.json',
+  './nam-192.png',
+  './nam-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
