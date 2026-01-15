5f19793bca38f31cc450b0e6be0a471972550c38
lsy9677-eng <lsy9677@gmail.com>
2026-01-16 01:13:41 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,13 +8,14 @@ const urlsToCache = [
   './nam-192.png',
   './nam-512.png'
 ];
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : Promise.resolve()))
-      )
-    )
-  );
-  self.clients.claim();
+  event.waitUntil(self.registration.unregister().then(() => {
+    return self.clients.matchAll().then(clients => {
+      clients.forEach(client => client.navigate(client.url));
+    });
+  }));
 });
