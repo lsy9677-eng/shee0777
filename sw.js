24fe7262984ec89fed93ab4e48410080ad79df56
lsy9677-eng <lsy9677@gmail.com>
2026-01-16 20:59:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,22 @@
-const CACHE_NAME = 'namyang-pwa-v2';
+// [수정] 버전 업그레이드 (v2 -> v3) & parent.html 추가
+const CACHE_NAME = 'namyang-pwa-v3';
 
 const urlsToCache = [
   './',
   './index.html',
-  './notice.html',
+  './parent.html',  // notice.html 대신 이거!
   './manifest.json',
   './nam-192.png',
   './nam-512.png'
 ];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
@@ -17,4 +26,12 @@ self.addEventListener('activate', (event) => {
     )
   );
   self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
